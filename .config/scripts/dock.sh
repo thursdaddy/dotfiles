77d336bdd7c518a8bfe1ae424c0f7cedc3a3cb9b
thursdaddy <thursdaddy@gmail.com>
2018-07-10 22:39:04 -0700

    added notification and corrected formatting

--- .config/scripts/dock.sh
+++ .config/scripts/dock.sh
@@ -4,19 +4,21 @@ MONITOR=$(xrandr --query | grep -w "connected" | awk '{print $1}' | grep -v eDP1
 
 
 dock () {
-  xrandr --output $MONITOR --auto --left-of eDP1
-  sleep 1
-  $HOME/.config/polybar/launch.sh &
+    xrandr --output $MONITOR --auto --left-of eDP1
+    sleep 1
+    $HOME/.config/polybar/launch.sh &
+    notify-send -t 5000 "$MONITOR has been enabled!"
 }
 
 undock () {
-	xrandr --output $MONITOR --off
+    xrandr --output $MONITOR --off
+    notify-send -t 5000 "$MONITOR has been disabled!"
 }
 
 case $1 in
   dock )
-	dock ;;
+    dock ;;
   undock )
-	undock ;;
+    undock ;;
 esac
 
