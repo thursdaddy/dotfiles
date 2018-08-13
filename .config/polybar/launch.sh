d29dee25703d295dde169ab9b6d20ddb55fa61c5
thursdaddy <thursdaddy@gmail.com>
2018-08-12 22:28:31 -0700

    polybar launch.sh reworked for less hacky multi-monitor support

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,23 +1,78 @@
 #!/usr/bin/env sh
 
-# Terminate already running bar instances
-killall -q polybar
+killem () {
+    killall -q polybar
+    while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+}
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+get_monitors () {
+    xrandr --query | grep -w "connected" | cut -f1 -d " " > /tmp/monlist
+}
 
-HDMI_CHECK=$(xrandr --query | grep -w "HDMI.* connected" | cut -d" " -f1)
-if [[ $HDMI_CHECK == HDMI* ]]; then
-	MONITOR=eDP1 polybar -q --reload side & 
-	MONITOR=$HDMI_CHECK polybar -q --reload main & 
-	nitrogen --restore
+turn_off () (
+    xrandr --query | grep -w "disconnected" | cut -f1 -d " " | grep -v eDP1
+)
+
+killem
+get_monitors
+
+if [[ $(wc -l < /tmp/monlist) -ge 3 ]]
+then
+    MULTI=1
 else
-	MONITOR=eDP1 polybar -q --reload main &
-	nitrogen --restore
+    MULTI=0
 fi
 
-DP_CHECK=$(xrandr --query | grep -w "DP.* connected" | cut -d" " -f1)
-if [[ $DP_CHECK == DP* ]]; then
-	MONITOR=$DP_CHECK polybar -q --reload side &
-	nitrogen --restore
-fi
+while read line
+do
+    MONITOR=$line
+    if [[ $MONITOR = 'eDP1' ]]; then
+        xrandr --output $MONITOR --auto
+        MONITOR=$MONITOR polybar -q --reload main &
+    fi
+    if [[ $MONITOR = 'DP2' ]]; then
+        if [ -z $1 ]; then
+            xrandr --output $MONITOR --auto --left-of eDP1
+            MONITOR=$MONITOR polybar -q --reload side &
+        else
+        xrandr --output $MONITOR --auto --$1-of eDP1
+        MONITOR=$MONITOR polybar -q --reload side &
+        fi
+    fi
+    if [[ $MONITOR = 'HDMI1' ]]; then
+        if [ -z $1 ]; then
+            if [[ $MULTI = '1' ]]; then
+                echo $MULTI $MONITOR LEFT OF $(xrandr --query | grep -w "connected" | cut -f1 -d " " | sed '1d;$d')
+                xrandr --output $MONITOR --auto --rotate left --left-of $(xrandr --query | grep -w "connected" | cut -f1 -d " " | sed '1d;$d')
+                MONITOR=$MONITOR polybar -q --reload vert &
+            elif [[ $MULTI = '0' ]]; then
+                echo $MULTI $MONITOR LEFT OF $(xrandr --query | grep -w "connected" | cut -f1 -d " " | head -n1)
+                xrandr --output $MONITOR --auto --left-of $(xrandr --query | grep -w "connected" | cut -f1 -d " " | head -n1)
+                MONITOR=$MONITOR polybar -q --reload side &
+            fi
+        else
+            if [[ $MULTI = '1' ]]; then
+                echo $MULTI $MONITOR $1 OF $(xrandr --query | grep -w "connected" | cut -f1 -d " " | sed '1d;$d')
+                xrandr --output $MONITOR --auto --rotate left --$1-of $(xrandr --query | grep -w "connected" | cut -f1 -d " " | sed '1d;$d')
+                MONITOR=$MONITOR polybar -q --reload side &
+            fi
+        echo $MULTI $MONITOR $1 OF $(xrandr --query | grep -w "connected" | cut -f1 -d " " | head -n1)
+        xrandr --output $MONITOR --auto --$1-of $(xrandr --query | grep -w "connected" | cut -f1 -d " " | head -n1)
+        MONITOR=$MONITOR polybar -q --reload side &
+        fi
+    fi
+    nitrogen --restore
+done < /tmp/monlist
+
+rm /tmp/monlist
+
+DIS_MON=$(turn_off)
+while IFS= read -r line
+do
+    MONITOR=$line
+    if [[ $MONITOR = 'DP2' ]]; then
+        xrandr --output $MONITOR --off
+    elif [[ $MONITOR = 'HDMI1' ]]; then
+        xrandr --output $MONITOR --off
+    fi
+done <<< $DIS_MON
